e3aba12fa7cccbc76f60d2139b0a3c5ff5c88f9b
Joey Chinoski <joeychin01@yahoo.com>
2020-06-24 20:22:05 +0000

    Added page with servlets to map from query string to recipe information and from recipe id to full recipe information

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,3 +11,23 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+function getRecipe(){
+  var numRecipe = document.getElementById("num-recipe").value;
+  fetch('/recipe?numRecipe='+numRecipe).then(response => response.json()).then((recipeInfo) => {
+    recipeInf = JSON.parse(recipeInfo);
+    const recipeDisplayElement = document.getElementById('recipe-info');
+    console.log(recipeInfo);
+    recipeDisplayElement.innerText = recipeInf["title"];
+  });
+}
+
+function getRecipeId(){
+  var dishName = document.getElementById("dish-name").value;
+  fetch('/dishId?dishName='+dishName).then(response => response.json()).then((recipeId) => {
+    recipe = JSON.parse(recipeId);
+    const recipeIdDisplayElement = document.getElementById('recipe-id-info');
+    console.log(recipeId);
+    recipeIdDisplayElement.innerText = recipe["results"];
+  });
+}
